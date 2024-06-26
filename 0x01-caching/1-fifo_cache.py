b7f3e8b876c2d9f41f06b6d8b0e5c7bf313d4a7e
KareemElkholi <kareemshrf@std.mans.edu.eg>
2024-06-26 22:36:01 +0300

    Some Tasks

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""1. FIFO caching"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """FIFOCache class"""
+    def put(self, key, item):
+        """Add an item in the cache"""
+        cache = self.cache_data
+        if key and item:
+            if len(cache) == self.MAX_ITEMS and key not in cache:
+                old_key = list(cache.keys())[0]
+                print(f"DISCARD: {old_key}")
+                del cache[old_key]
+            if key in cache:
+                del cache[key]
+            cache[key] = item
+
+    def get(self, key):
+        """Get an item by key"""
+        return self.cache_data.get(key)
